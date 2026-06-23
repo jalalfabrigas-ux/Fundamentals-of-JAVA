78003b8a08b38272854e784e87da1f4bbf67aef4
JALAL Likes Coffee <jalalfabrigas@gmail.com>
2026-06-23 13:11:24 +0000

    How to Print in JAVA

---
+++ SystemPrint/SystemPrint.java
@@ -0,0 +1,9 @@
+public class SystemPrint{
+    public static void main(String[]args){
+        
+
+        System.out.print("HELLO JALAL");
+        System.out.println();
+        System.out.println("Likes Coffees");
+    }
+}
