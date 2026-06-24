c8267a64118f864734c8fedcadeca2916268992c
JALAL Likes Coffee <jalalfabrigas@gmail.com>
2026-06-24 13:22:59 +0000

    Declaration of Variable

---
+++ VariableDeclaration/Variable.java
@@ -0,0 +1,17 @@
+package VariableDeclaration;
+
+public class Variable {
+    public static void main(String[] args){
+
+        String name = "JALAL";
+        char middleInitial = 'F';
+        int age = 19;
+        double favNum = 4.14;
+
+        System.out.print("\nNAME: " + name);
+        System.out.print("\nInitial: " + middleInitial);
+        System.out.print("\nAge: " + age);
+        System.out.print("\nFAVORITE NUMBER: " + favNum);
+    }
+    
+}
