080f655842633f61d19910b2cc6304eee6067b6e
JALAL Likes Coffee <jalalfabrigas@gmail.com>
2026-06-25 13:56:19 +0000

    if else if

---
+++ DecisionControlStructure/ifElse.java
@@ -0,0 +1,15 @@
+package DecisionControlStructure;
+public class ifElse {
+    public static void main(String[] args){
+
+        int number = 15;
+        
+        if(number <= 15){
+            System.out.print("\n THE PROGRAM CHOOSE A");
+        }else{
+            System.out.print("\n THE PROGRAM CHOOSE B");
+        }
+
+    }
+    
+}
