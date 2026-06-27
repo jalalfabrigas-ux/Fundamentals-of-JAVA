2418588052fcff1f9f2073762e5dcc2c2646e469
JALAL Likes Coffee <jalalfabrigas@gmail.com>
2026-06-27 11:51:36 +0000

    switch

---
+++ DecisionControlStructure/Switch.java
@@ -0,0 +1,23 @@
+package DecisionControlStructure;
+public class Switch{
+    public static void main(String args[]){
+
+        int example=2;
+
+        switch(example){
+            case 1:
+                System.out.print("CASE 1");
+                break;
+            case 2:
+                System.out.print("CASE 2");
+                break;
+            case 3:
+                System.out.print("CASE 3");
+                break;
+            default:
+                System.out.print("OPERATION ERROR!");
+                break;
+            }
+
+    }
+}
